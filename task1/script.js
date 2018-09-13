f14c760210219dd84cf6e9efa83120b0d00f1d0d
vladimirshab <vvs7272@mail.ru>
2018-09-13 11:33:41 +0300

    1

---
+++ task1/script.js
@@ -0,0 +1,15 @@
+﻿var prost = [ 2 ],
+    chislo = 2;
+while ( chislo <= 100) {
+    var chislo_ver = 1;
+    for ( var i = 0; i < prost.length; i++) {
+        if ( chislo % prost[ i ] === 0) {
+            chislo_ver = 0;
+        }
+    }
+    if ( chislo_ver === 1 ) {
+        prost.push( chislo );
+    }
+    chislo++;
+}
+console.log ( prost );
